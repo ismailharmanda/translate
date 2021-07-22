61e9075946c2ec9b420b15df8b8344bc83def61c
ismail harmanda <iharmanda@hotmail.com>
2021-07-22 14:34:03 +0300

    implement created LanguageContext

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,10 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
   render() {
+    console.log(this.context);
     return <button className="ui primary">Submit</button>;
   }
 }
